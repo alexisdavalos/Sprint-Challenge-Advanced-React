f83c8d5eab462460cfacdb6c881f25bb6bd0d647
Alexis <alexisdavalos.tech@gmail.com>
2020-01-18 11:17:08 -0500

    Installed Dependencies and set up Class Component: App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  //Component Constructor Function
+  constructor(){
+    super()
+    this.state = {
+      state: ''
+    }
+  }
+  //Component Life Cycle Mount Function
+  componentDidMount(){
+    
+  }
+  //Renders App
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
